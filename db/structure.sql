ae7c331725abcbbef74a39f950585926b48c35d0
Alexander Tumin <iamtakingiteasy@eientei.org>
2021-12-17 23:49:07 +0300

    post-merge edits

--- db/structure.sql
+++ db/structure.sql
@@ -10,20 +10,6 @@ SET client_min_messages = warning;
 SET row_security = off;
 
 --
--- Name: plpgsql; Type: EXTENSION; Schema: -; Owner: -
---
-
-CREATE EXTENSION IF NOT EXISTS plpgsql WITH SCHEMA pg_catalog;
-
-
---
--- Name: EXTENSION plpgsql; Type: COMMENT; Schema: -; Owner: -
---
-
-COMMENT ON EXTENSION plpgsql IS 'PL/pgSQL procedural language';
-
-
---
 -- Name: post_status; Type: TYPE; Schema: public; Owner: -
 --
 
@@ -316,6 +302,8 @@ CREATE FUNCTION public.user_logs_touch(new_user_id integer, new_ip inet) RETURNS
 
 
 SET default_tablespace = '';
+
+SET default_table_access_method = heap;
 
 --
 -- Name: advertisements; Type: TABLE; Schema: public; Owner: -
@@ -1117,7 +1105,8 @@ CREATE TABLE public.posts (
     frames_pending text DEFAULT ''::text NOT NULL,
     frames_warehoused boolean DEFAULT false NOT NULL,
     updated_at timestamp with time zone,
-    tags_array character varying[]
+    tags_array character varying[],
+    artist_commentary character varying DEFAULT ''::character varying
 );
 
 
@@ -3407,11 +3396,13 @@ INSERT INTO "schema_migrations" (version) VALUES
 ('20160330063707'),
 ('20180624074601'),
 ('20190518111956'),
+('20190519200200'),
 ('20190817070727'),
 ('20191110172526'),
 ('20200908180652'),
 ('20201103140508'),
 ('20210211213304'),
+('20211217233407'),
 ('21'),
 ('22'),
 ('23'),
